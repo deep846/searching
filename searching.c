55220dc6677174e785d09b53402805314af9d228
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-08 14:23:22 +0530

    searching

---
+++ searching.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int searching(int *x,int n,int key)
+{
+    int i;
+    for(i=0; i<n; i++)
+    {
+        if(x[i]==k in the list ");
+}
+int main()
+{
+    int x[10],n,key,i;
+    printf("\n Enter the no of array element");
+    scanf("%d",&n);
+    for(i=0key element :- ");
+    scanf("%d",&key);
+    searching(x,n,key);
+    getch();
+}
