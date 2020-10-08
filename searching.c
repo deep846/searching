c46861ac1208075e49ad2b63607f07e6c545bcee
Lightning@Emperor21 <61249444+NorahC03@users.noreply.github.com>
2020-10-08 14:45:17 +0530

    solved issue

--- searching.c
+++ searching.c
@@ -6,14 +6,26 @@ int searching(int *x,int n,int key)
     int i;
     for(i=0; i<n; i++)
     {
-        if(x[i]==k in the list ");
+        if(x[i]==key)
+        {
+            printf("\n Element is found  ");
+            break;
+        }
+    }
+    if(i==n)
+        printf("\n Element is not found in the list ");
 }
 int main()
 {
     int x[10],n,key,i;
     printf("\n Enter the no of array element");
     scanf("%d",&n);
-    for(i=0key element :- ");
+    for(i=0; i<n; i++)
+    {
+        printf("\n Enter the %d array element ",i);
+        scanf("%d",&x[i]);
+    }
+    printf("\n Enter the key element :- ");
     scanf("%d",&key);
     searching(x,n,key);
     getch();
